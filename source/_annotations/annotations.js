4a194c4c5811b9a042698b8d8e13d09e44fcb0de
Brad Busenius <bradb@neomailbox.ch>
2016-12-01 19:11:09 -0600

    Initial commit

---
+++ source/_annotations/annotations.js
@@ -0,0 +1,14 @@
+{
+  "comments" : [
+    {
+      "el": "#annotation-css-selector",
+      "title" : "Annotation title",
+      "comment": "Annotation description"
+    },
+    {
+      "el": ".blockquotefoo",
+      "title" : "Blockquote FOO YOU FOOBAR",
+      "comment": "Wtf? I'm just testing this"
+    }
+  ]
+}
---
+++ source/_patterns/00-atoms/01-text/headings.md
@@ -0,0 +1,5 @@
+---
+title: Headings Fooobar you
+---
+
+[Insert description here]
